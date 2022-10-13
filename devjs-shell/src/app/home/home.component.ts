3c79b23758b81d145614574718803c3787e4464b
Rafal Brzoska <rafal@brzoska.net.pl>
2022-10-13 11:34:54 +0200

    signgle dynamic ocomponent

--- devjs-shell/src/app/home/home.component.ts
+++ devjs-shell/src/app/home/home.component.ts
@@ -6,14 +6,11 @@ import { ComponentLoaderService } from '../component-loader.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   @ViewChild('dynamicContainer', { static: true, read: ViewContainerRef }) dynamicContainer!: ViewContainerRef;
   constructor(private _componentLoader: ComponentLoaderService) { }
 
-  ngOnInit(): void {
-    
-  }
 
   loadComponent(name: string) {
     this.dynamicContainer.clear();
